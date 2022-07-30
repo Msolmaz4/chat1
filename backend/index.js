8d5d26631b5664475e4d5b9b42c1e348c06d3390
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-07-30 03:34:25 +0200

    sendmessage

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,10 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const cors = require('cors')
-// burada server yapariz socket io cekeriz fonksiyon yazarak 
-//IO server acariy karsi tarafada
 
+// burada server yapariz socket io cekeriz fonksiyon yazarak 
+//IO server acariy karsi tarafadacors acizoruy sonra cors tanimliyoruy
+const {Server} = require('socket.io')
 app.use(cors())
 
 const server = http.createServer(app)
@@ -18,6 +19,29 @@ const io = new Server(server,{
         methods:['GET','POST']
     }
 })
+
+//baglantiyi karsizya gondermek oldugumuy kontrol vr foksyon yayariy
+io.on('connection' ,(socket)=>{
+    console.log(`connection#;${socket.id}`)
+
+//odaya baglanm{ak icn bir foks yayariy
+socket.on('join_room' ,(data)=>{
+    socket.join(data)
+    console.log(`user id:${socket.id} join room:${data}`)
+})  
+
+// messai almak icin
+
+            
+    socket.on('send_message',(data)=>{
+        console.log(data)
+    })
+
+//baglantiyi koparmak icinde bunu yazariy
+    socket.on('disconect',()=>{
+        console.log('disconnect',socket.id)
+    })
+} )
 
 server.listen(3001,()=>{
     console.log('server ruu')
